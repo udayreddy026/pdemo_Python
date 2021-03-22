3d188cea7752f086dce8a05a07348cb64baffc9c
personal <udayreddy026@gmail.com>
2021-03-22 11:12:11 +0530

    New File Added condition_Statements1.py

---
+++ Vijay_Sir/condition_statements2.py
@@ -0,0 +1,15 @@
+x = 20
+y = 20
+if x + y == x - y:
+    print('True!')
+    if x * y == x <= y:
+        print('True! True!')
+elif x == y or x <= y:
+    if x == 200 // 10:
+        print('Welcome', x)
+    if x >= y:
+        print("yes")
+    else:
+        print("Bye")
+else:
+    print("Gud Bye....")
