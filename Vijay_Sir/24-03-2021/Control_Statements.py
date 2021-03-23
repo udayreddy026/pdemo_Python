081717aaef241b4cf3fc8d53244e0cb2fdd25e7e
personal <udayreddy026@gmail.com>
2021-03-23 12:01:35 +0530

    New Direct added and control and loop_conditional_statements added

---
+++ Vijay_Sir/24-03-2021/Control_Statements.py
@@ -0,0 +1,22 @@
+# Printing 1 to 20 Number Using While loop
+
+a = 1
+while a <= 20:
+    print(a, end=",")
+    a += 1
+
+
+# Printing -7, -5, -3, -1
+
+x = -7
+while x <= -1:
+    print(x,)
+    x = x + 2
+
+
+# Printing 5, 4, 3, 2, 1
+
+a = 5
+while a >= 1:
+    print(a)
+    a = a - 1
