27ae09cccdfe5b47d634da77c0d8c46e25f535ad
personal <udayreddy026@gmail.com>
2021-03-23 12:02:11 +0530

    New Direct added and control and loop_conditional_statements added

---
+++ Vijay_Sir/24-03-2021/Loops_Conditional_Statements.py
@@ -0,0 +1,30 @@
+# Divided 5 Numbers
+"""n = 1
+while n <= 20:
+    if n % 5 == 0:
+        print(n,)
+    n = n + 1
+    """
+# Print Even Number from 1 to 50:
+
+"""e = 1
+while e <= 50:
+    if e % 2 == 0:
+        print(e)
+    e = e + 1"""
+
+# Print Odd Number from 1 to 50:
+
+"""e = 1
+while e <= 50:
+    if e % 2 == 1:
+        print(e)
+    e = e + 1"""
+
+# Print Divided by 7 Number from 1 to 50:
+
+e = 1
+while e <= 50:
+    if e % 7 == 0:
+        print(e)
+    e = e + 1
