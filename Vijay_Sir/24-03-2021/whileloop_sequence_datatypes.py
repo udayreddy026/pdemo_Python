8aa7e8cf3cd4e089b831e054930a947d462f674e
personal <udayreddy026@gmail.com>
2021-03-24 12:22:18 +0530

    Added New Dir 24-03-2021 and whileloop_sequence_datatypes.py file also added

---
+++ Vijay_Sir/24-03-2021/whileloop_sequence_datatypes.py
@@ -0,0 +1,80 @@
+"""x = [10, 20, 30]
+# We need to print 10 and 30 using while loop
+
+i = 0
+while i <= 2:
+    print(x[i])
+    i = i + 2"""
+
+"""x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# We need to print 2,4,6 and 8 using while loop
+
+i = 1
+while i <= 8:
+    print(x[i])
+    i = i + 2
+"""
+
+# Using len() Method in while loop
+
+"""x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# We need to print 2,4,6 and 8 using while loop
+
+i = 1
+while i <= len(x) - 1:
+    print(x[i])
+    i = i + 2
+"""
+
+"""x = [23, 36, 45, 32, 64]
+# We need to print 23 and 45 using while loop
+
+i = 0
+while i <= len(x) - 3:
+    print(x[i])
+    i = i + 2"""
+
+"""x = [24, 35, 63, 72, 98, 45]
+# We need to print 23 and 45 using while loop
+
+i = 0
+while i <= len(x) - 3:
+    print(x[i])
+    i = i + 1"""
+
+# While Loop with strings
+
+"""s = 'palle'
+
+i = 0
+
+while i <= len(s)-1:
+    print(s[i], end=",")
+    i = i + 1"""
+
+# Reversing the string
+
+"""s = 'palle'
+i = len(s) - 1
+while i >= 0:
+    print(s[i], end=",")
+    i = i - 1"""
+
+# Given list is [10,20,30,40,50] we need to print onli [50,30,10]
+
+"""x = [10, 20, 30, 40, 50]
+i = len(x) - 1
+while i >= 0:
+    print(x[i])
+    i = i - 2"""
+
+# Finding Even Number
+"""x = [10, 20, 33, 43, 21, 41, 51, 61, 93, 71]
+i = 0
+while i <= len(x)-1:
+    if x[i] % 2 == 0:
+        print(x[i])
+    i = i + 1"""
+
+# Natural Numbers
+
