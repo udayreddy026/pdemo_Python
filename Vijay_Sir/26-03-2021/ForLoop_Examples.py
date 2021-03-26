a5f8dba9f42c43c663154c21bbe5b6b1f43bd48e
udayreddy026 <udayreddy026gmail.com>
2021-03-26 13:32:47 +0530

    One More New Dir added & New file added i.e.. ForLoop_Examples.py

---
+++ Vijay_Sir/26-03-2021/ForLoop_Examples.py
@@ -0,0 +1,33 @@
+# Printing List elements in reverse order!!!!
+
+# y = [32, 24, 73, 64]
+# for i in range(len(y)-1, -1, -1):
+#     print(y[i])
+
+
+# Finding the Vowels in given string!!!
+
+# x = 'Palle'
+# for y in x:
+#     if y == 'a' or y == 'e' or y == 'i' or y == 'o' or y == 'u':
+#         print(y)
+
+# Finding the Vowels in given string using in  Operator!!!
+
+for i in range(1, 6):
+    if i == 2:
+        break
+    print("I Values:", i)
+
+
+for i in range(1, 6):
+    if i == 4:
+        continue
+    print("I values is :::", i)
+
+i = 20
+while i < 10:
+    pass
+print("Welcome")
+
+
