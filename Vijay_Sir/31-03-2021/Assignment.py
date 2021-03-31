c50ad2cfae943896dd4f3e11b0f329dd66b19bcd
udayreddy026 <udayreddy026gmail.com>
2021-03-31 18:00:42 +0530

    Added New File Assignment.py

---
+++ Vijay_Sir/31-03-2021/Assignment.py
@@ -0,0 +1,29 @@
+# Prime or Not
+
+# def prime(num1):
+#     if num1 > 1:
+#         # return True
+#         for i in range(2, num1):
+#             if num1 % 2 == 0 or num1 % 3 == 0:
+#                 return False
+#             else:
+#                 return True
+#
+#
+# num = int(input("Enter the number:::"))
+# Res = prime(num)
+# print(Res)
+
+
+def Details(name, age, phone_no, city='Bengaluru'):
+    print("Name:", name, "\n",
+          "Age:", age, "\n",
+          "PhoneNo:", phone_no, "\n",
+          "City:", city, "\n")
+
+
+Details('Uday', 22, 9703654029,)
+Details('Esxs', 32, 7875278258, 'ASDAFd')
+Details('Esxs', 32, 7875278258, 'ASDAFd')
+Details('Esxs', 32, 7875278258, 'ASDAFd')
+
