da773dfb5a2eb356f4314a05f8be4518827d3ba8
udayreddy026 <udayreddy026gmail.com>
2021-04-05 22:35:49 +0530

    Added dir 05-04-2021 & Module added 3 files in that dirs add.py, sec.py,kwargs.py

---
+++ Vijay_Sir/05-04-2021/Modules/add.py
@@ -0,0 +1,18 @@
+def sum_1(a, b):
+    return a + b
+
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+print("Some of two numbers is:::", sum_1(x, y))
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        return "Its a even Number"
+    else:
+        return "Its a odd Number"
+
+
+x = int(input("Enter a number:::"))
+print(even_odd(x))
---
+++ Vijay_Sir/05-04-2021/kwargs.py
@@ -0,0 +1,7 @@
+# Keyword arbitrary Parameters...!
+def user(**a):
+    print(a)
+
+
+user(Name="Uday", PhoneNo=9703654029)
+user(Name="Poorna", Phno=7981671143, Gender="Male", Age=20, Qulification="Degree")
