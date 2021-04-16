483dd9f9bba4b57fe6a8793fbaca8c6f7f21dff0
udayreddy026 <udayreddy026gmail.com>
2021-04-16 16:58:07 +0530

    Banking application Program

---
+++ Vijay_Sir/16-04-2021/Banking_application.py
@@ -0,0 +1,34 @@
+class Bank:  #Class
+    bank_total_amount = 0 # Class Variable
+
+    def __init__(self, account_number, account_balance, pan_number):  # Constructor with local variables
+        self.ac_number = account_number  # ac_number is instance Variable
+        self.ac_balance = account_balance
+        self.pan_number = pan_number
+        Bank.bank_total_amount = Bank.bank_total_amount + self.ac_balance
+
+    def credit(self, debited_money): # Method credit
+        self.ac_balance = self.ac_balance + debited_money
+        Bank.bank_total_amount = Bank.bank_total_amount + debited_money
+
+    def debited(self, withdraws_money):
+        if self.ac_balance >= 500 :
+            self.ac_balance = self.ac_balance - withdraws_money
+            Bank.bank_total_amount = Bank.bank_total_amount - withdraws_money
+        elif self.ac_balance <= 500:
+            print("Low Balances")
+
+    def balance(self):
+        print("Account Balances is:", self.ac_balance)
+
+
+uday = Bank(1025423, 5000, 'TU10324D')
+jock = Bank(10254213, 123000, 'UA25324C')
+
+print("Uday Bank Balance:", uday.ac_number, uday.ac_balance)
+print("Uday Bank Balance:", jock.ac_number, jock.ac_balance)
+print()
+
+uday.credit(15000)
+uday.balance()
+
--- logical_aptitude/14-04-2021/Finding_possition.py
+++ logical_aptitude/14-04-2021/Finding_possition.py
@@ -1,9 +1,9 @@
-name = 'Palle technologies'
-for e in range(len(name)-1, -1, -1):
-    if name[e] == 'e':
-        print(e)
-        break
-
+# name = 'Palle technologies'
+# for e in range(len(name)-1, -1, -1):
+#     if name[e] == 'e':
+#         print(e)
+#         break
+#
 
 # Not Working
 # for e in range(len(name)):
@@ -16,6 +16,15 @@ for e in range(len(name)-1, -1, -1):
 #         break
 
 # friends = ['Naveen', 'Pavan', 'Arun', 'Balaji', 'Ashok', 'Riyaz', 'Yadham Raju', 'Hari']
+# for i in friends:
+#     for len in range(1, len(friends)):
+#         if len(i) <= len(friends(len)):
+#             print(friends)
+
+d = {'name': 'uday', 'asdasd':12416513, }
+print(d)
+print(type(d))
+
 # friends.sort()
 # for names in friends:
 #     print(names)
