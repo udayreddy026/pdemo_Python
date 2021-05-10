a7f51b8b4294f7ad2ceeb8e645c9835bf0026d04
udayreddy026 <udayreddy026gmail.com>
2021-05-10 21:03:12 +0530

    file name changes

--- Vijay_Sir/03-05-2021/Files_Concepts.py
+++ Vijay_Sir/03-05-2021/Files_Concepts.py
@@ -1,17 +1,17 @@
 # # -------------------------- Reading File Concepts ------------------------
-# f = open('E:/pdemo_Python/Vijay_Sir/03-04-2021/demo.txt', 'r')
+# f = open('E:/pdemo_Python/Vijay_Sir/03-05-2021/demo.txt', 'r')
 # a = f.read()
 # print(a)
 #
 # # ----------------------- readline method -----------------------
-# f = open('E:/pdemo_Python/Vijay_Sir/03-04-2021/demo.txt', 'r')
+# f = open('E:/pdemo_Python/Vijay_Sir/03-05-2021/demo.txt', 'r')
 # # a = f.readline()
 # print(f.readline())
 # print(f.readline())
 # print(f.readline())
 #
 # # ---------------------- readlines method --------------------------
-# f = open('E:/pdemo_Python/Vijay_Sir/03-04-2021/demo.txt', 'r')
+# f = open('E:/pdemo_Python/Vijay_Sir/03-05-2021/demo.txt', 'r')
 # a = f.readlines()
 # print(a)
 # print(a[2])
@@ -22,11 +22,11 @@
 
 # -------------------------- Writing File Concept ---------------------------
 # ---------- We can using write mode it will erases existing data in file ---------
-# f = open('E:/pdemo_Python/Vijay_Sir/03-04-2021/write.txt', 'w')
+# f = open('E:/pdemo_Python/Vijay_Sir/03-05-2021/write.txt', 'w')
 # v = f.writelines('uday')
 # print(v)
 
-f = open('E:/pdemo_Python/Vijay_Sir/03-04-2021/write.txt', 'a')
+f = open('/Vijay_Sir/03-05-2021/write.txt', 'a')
 f.writelines('\n')
 f.writelines('uday reddy')
 
